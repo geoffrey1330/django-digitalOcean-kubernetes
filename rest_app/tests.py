504c4e73318e2d2e2fc32982c05bdb3184b40d80
Geoffrey Israel <israelgeoffrey13@gmail.com>
2022-04-19 02:43:33 +0100

    k8s deployment in github actions

--- rest_app/tests.py
+++ rest_app/tests.py
@@ -1,3 +1,13 @@
 from django.test import TestCase
+from . models import Article
 
-# Create your tests here.
+
+class ModelTesting(TestCase):
+
+    def setUp(self):
+        self.article = Article.objects.create(title='boy', author='michael')
+
+    def test_contact_model(self):
+        d = self.article
+        self.assertTrue(isinstance(d, Article))
+        self.assertEqual(str(d), 'boy')
