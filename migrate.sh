e95e0bffc8becdf62036d7d83fc72adf244426e9
Geoffrey Israel <israelgeoffrey13@gmail.com>
2022-04-19 02:48:26 +0100

    k8s deployment in github actions

--- migrate.sh
+++ migrate.sh
@@ -4,4 +4,4 @@ SUPERUSER_EMAIL=${DJANGO_SUPERUSER_EMAIL:-"abc@gmail.com"}
 cd /app/
 
 /opt/venv/bin/python manage.py migrate --noinput
-/opt/venv/bin/python manage.py createsuperuser --email $SUPERUSER_EMAIL --noinput || true
+#/opt/venv/bin/python manage.py createsuperuser --email $SUPERUSER_EMAIL --noinput || true
